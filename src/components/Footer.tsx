f71ba226057f81f46d17a4cbc308f40516d4bdf1
nikewidestudio-afk <nikewidestudio@gmail.com>
2026-01-05 15:37:05 +0000

    changes made on the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import {HugeiconsIcon, } from  '@hugeicons/react';
 export default function Footer() {
     return (
         <footer className="w-full h-[470px] bg-[#7FFBAE] flex flex-col items-center justify-center">
-        <div className="flex items-center justify-center w-[90%] border-b border-gray-500 pb-10">
+        <div className="flex items-center justify-center w-[90%] border-b border-gray-400 pb-10">
            <div className="flex w-full items-center justify-between">
            <div>
             <h2 className="text-[20px] font-bold text-[#5B5B5B]">
@@ -24,46 +24,43 @@ export default function Footer() {
             <div className="flex flex-col">
                 <h3 className='text-[20px] font-bold'>Home</h3>
                 <ul className='flex flex-col mt-5'>
-                    <li>About</li>
-                    <li>Case Studies</li>
-                    <li>Our Projects</li>
-                    <li>Blogs</li>
+                    <li className='my-1 cursor-pointer'>About</li>
+                    <li className='my-1 cursor-pointer'>Case Studies</li>
+                    <li className='my-1 cursor-pointer'>Our Projects</li>
+                    <li className='my-1 cursor-pointer'>Blogs</li>
                 </ul>
             </div>
             <div className="flex flex-col">
                 <h3 className='text-[20px] font-bold'>About</h3>
                 <ul className='flex flex-col mt-5'>
-                    <li>About</li>
-                    <li>Case Studies</li>
-                    <li>Our Projects</li>
-                    <li>Blogs</li>
+                    <li className='my-1 cursor-pointer'>About</li>
+                    <li className='my-1 cursor-pointer'>Case Studies</li>
+                    <li className='my-1 cursor-pointer'>Our Projects</li>
+                    <li className='my-1 cursor-pointer'>Blogs</li>
                 </ul>
             </div>
             <div className="flex flex-col">
                 <h3 className='text-[20px] font-bold'>Services</h3>
                 <ul className='flex flex-col mt-5'>
-                    <li>SEO</li>
-                    <li>Project Manager</li>
-                    <li>Social Media</li>
-                    <li>See All</li>
+                    <li className='my-1 cursor-pointer'>SEO</li>
+                    <li className='my-1 cursor-pointer'>Project Manager</li>
+                    <li className='my-1 cursor-pointer'>Social Media</li>
+                    <li className='my-1 cursor-pointer'>See All</li>
                 </ul>
             </div>
             <div className="flex flex-col">
                 <h3 className='text-[20px] font-bold'>For Companies</h3>
                 <ul className='flex flex-col mt-5'>
                     <li className='text-[#5B5B5B] font-normal text-[20px]'>Work with us</li>
-                    <li>Contact us</li>
-                    <li>Contact us</li>
-                    <li>Contact us</li>
+                    <li className='my-1 cursor-pointer'>Contact us</li>
+                    <li className='my-1 cursor-pointer'>Contact us</li>
+                    <li className='my-1 cursor-pointer'>Contact us</li>
                 
                 </ul>
             </div>
 
            </div>
         </div>
-
-            
-
         <p className="text-[#5B5B5B] font-normal text-[15px] mt-10">© 2024 Antartes. All rights reserved.</p>
         </footer>
     )
